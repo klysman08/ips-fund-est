4cf923948dff6a690a75aff2e7dc30d3805adc17
klysman08 <klysman.rk@gmail.com>
2023-10-25 21:32:18 +0100

    up

---
+++ LAB 2/Exercicio 1.R
@@ -0,0 +1,48 @@
+# Defina o vetor de notas
+notas <- c(9, 15, 15, 17, 14, 16, 19, 10, 14, 15, 18, 16, 13, 17)
+
+# 1) Tabela de frequências
+tabela_freq <- table(notas)
+print(tabela_freq)
+
+# 2) Média
+media <- mean(notas)
+print(paste("Média:", media))
+
+# Mediana
+mediana <- median(notas)
+print(paste("Mediana:", mediana))
+
+# Desvio padrão
+desvio_padrao <- sd(notas)
+print(paste("Desvio Padrão:", desvio_padrao))
+
+# Mínimo
+minimo <- min(notas)
+print(paste("Mínimo:", minimo))
+
+# Máximo
+maximo <- max(notas)
+print(paste("Máximo:", maximo))
+
+# Amplitude total
+amplitude_total <- maximo - minimo
+print(paste("Amplitude Total:", amplitude_total))
+
+# Quartis
+quartis <- quantile(notas, probs = c(0.25, 0.5, 0.75))
+print(paste("1º Quartil (Q1):", quartis[1]))
+print(paste("2º Quartil (Mediana):", quartis[2]))
+print(paste("3º Quartil (Q3):", quartis[3]))
+
+# Quantil de 45%
+quantil_45 <- quantile(notas, probs = 0.45)
+print(paste("Quantil de 45%:", quantil_45))
+
+# 3) Histograma
+hist(notas, main = "Histograma de Notas", xlab = "Notas", ylab = "Frequência", col = "lightblue")
+
+# Box-plot
+boxplot(notas, main = "Box Plot de Notas", ylab = "Notas")
+
+
---
+++ LAB 2/Exercicio 2.R
@@ -0,0 +1,55 @@
+# Temperaturas máximas diárias registradas
+temperaturas <- c(24.6, 25.2, 26.6, 25.9, 27.4, 28.5, 30.0, 30.4, 30.4, 28.9, 27.1, 27.8, 28.9, 29.1, 28.8, 27.2, 26.8, 27.9, 30.1, 27.1, 24.0, 23.1, 24.5, 25.2, 27.0, 27.2, 26.2, 25.6, 23.5, 24.0, 25.7)
+
+# a) Quantas temperaturas foram registradas?
+num_temperaturas <- length(temperaturas)
+cat("a) Quantidade de temperaturas registradas:", num_temperaturas, "\n")
+
+# b) Qual a temperatura máxima registada? E a mínima?
+temp_maxima <- max(temperaturas)
+temp_minima <- min(temperaturas)
+cat("b) Temperatura máxima registrada:", temp_maxima, "\n")
+cat("   Temperatura mínima registrada:", temp_minima, "\n")
+
+# c) Qual a temperatura média?
+temp_media <- mean(temperaturas)
+cat("c) Temperatura média:", temp_media, "\n")
+
+# d) A dispersão das temperaturas relativamente à temperatura média é medida pelo desvio padrão.
+dispersao <- sd(temperaturas)
+cat("d) A dispersão das temperaturas relativamente à temperatura média é", dispersao, "\n")
+
+# e) Ordene a amostra
+temperaturas_ordenadas <- sort(temperaturas)
+cat("e) Amostra ordenada:", temperaturas_ordenadas, "\n")
+
+# f) Obtenha a mediana e interprete o resultado
+mediana <- median(temperaturas)
+cat("f) Mediana:", mediana, "\n")
+cat("   A mediana é o valor que divide a amostra ao meio, ou seja, 50% das temperaturas estão abaixo dela e 50% estão acima dela. No caso, a mediana representa o valor central da amostra ordenada.\n")
+
+# g) Obtenha o histograma das temperaturas e analise-o
+hist(temperaturas, main = "Histograma das Temperaturas", xlab = "Temperatura (°C)", ylab = "Frequência", col = "lightblue")
+# Análise: O histograma mostra a distribuição das temperaturas em relação à frequência. Pode-se observar a forma da distribuição a partir do histograma.
+
+# h) Obtenha a box-plot e analise-a
+boxplot(temperaturas, main = "Box Plot das Temperaturas", ylab = "Temperatura (°C)")
+# Análise: A box-plot fornece informações sobre a mediana, os quartis e os valores discrepantes (outliers). Pode ajudar a identificar a presença de valores extremos na amostra.
+
+# i) Relativamente à simetria/assimetria, a distribuição das temperaturas é:
+# Para avaliar a simetria/assimetria, você pode usar o gráfico de histograma ou uma medida de assimetria como a assimetria de Pearson ou a assimetria de Fisher. Não é possível determinar a simetria/assimetria sem ver a distribuição real.
+
+# j) Obtenha a tabela de frequências das temperaturas
+tabela_freq <- table(temperaturas)
+print("j) Tabela de Frequências:")
+print(tabela_freq)
+
+# k) Agrupe os dados das temperaturas em 4 classes
+classes_4 <- cut(temperaturas, breaks = 4, include.lowest = TRUE)
+print("k) Dados agrupados em 4 classes:")
+print(table(classes_4))
+
+# l) Agrupe os dados das temperaturas em 5 classes
+classes_5 <- cut(temperaturas, breaks = 5, include.lowest = TRUE)
+print("l) Dados agrupados em 5 classes:")
+print(table(classes_5))
